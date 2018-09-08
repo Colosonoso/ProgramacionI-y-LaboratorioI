746166f9b8b2af494b3110d6901455f491bda972
Colosonoso <36230695+Colosonoso@users.noreply.github.com>
2018-09-07 22:15:27 -0300

    Add files via upload

---
+++ Vectores/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 5
+
+int main()
+{
+
+    int vector[TAM]={50,35,78,22,7}
+    int indice;
+
+    indice = buscarNumero(vector,TAM,)
+
+
+
+
+    return 0;
+}
+void mostrarVector(int x[], int tam )
+   {
+
+   }
